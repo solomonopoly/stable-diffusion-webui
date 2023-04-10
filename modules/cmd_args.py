34319323ace3130e3f98e345ab7682a1c736473c
btian <ben.tian@graviti.cn>
2023-04-10 12:45:52 +0800

    skip to load model at startup to reduce launch time

--- modules/cmd_args.py
+++ modules/cmd_args.py
@@ -106,3 +106,4 @@ parser.add_argument("--monitor-system-calls", action='store_true', help="monitor
 parser.add_argument("--system-monitor-addr", type=str,  help="a remote server who can monitor gpu calls", default='')
 parser.add_argument("--system-monitor-api-secret", type=str,  help="api secret fo calling monitor api", default='')
 parser.add_argument("--enable-insecure-calls", action='store_true',  help="enable insecure calls after related UI component is hidden", default=False)
+parser.add_argument("--skip-load-default-model", action='store_true',  help="skip to load model at startup to reduce launch time", default=False)
