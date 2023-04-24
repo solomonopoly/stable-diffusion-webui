bbb1a9a9f6cdd2bcc7fa1da0ce623c67336b7e90
Edward Cui <edward.cui@graviti.cn>
2023-04-24 17:44:17 +0800

    Fix model seach after adding upload button
    The model upload button has broke the search functionality. Change the
    js code to ignore the upload button.
    
    Also fix the metadata propogation problem.

--- extensions-builtin/Lora/ui_extra_networks_lora.py
+++ extensions-builtin/Lora/ui_extra_networks_lora.py
@@ -19,17 +19,26 @@ class ExtraNetworksPageLora(ui_extra_networks.ExtraNetworksPage):
         for name, lora_on_disk in lora.available_loras.items():
             path, ext = os.path.splitext(lora_on_disk.filename)
             metadata_path = "".join([path, ".meta"])
+            metadata = ui_extra_networks.ExtraNetworksPage.read_metadata_from_file(metadata_path)
+            search_term = self.search_terms_from_path(lora_on_disk.filename)
+            if metadata is not None:
+                search_term = " ".join([
+                    search_term,
+                    ", ".join(metadata["tags"]),
+                    ", ".join(metadata["trigger_word"]),
+                    metadata["model_name"],
+                    metadata["sha256"]])
             yield {
                 "name": name,
                 "filename": path,
                 "preview": self.find_preview(path),
                 "description": self.find_description(path),
-                "search_term": self.search_terms_from_path(lora_on_disk.filename),
-                "prompt": json.dumps(f"<lora:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
+                "search_term": search_term,
+                "prompt": json.dumps(
+                    f"<lora:{name}:") + " + opts.extra_networks_default_multiplier + " + json.dumps(">"),
                 "local_preview": f"{path}.{shared.opts.samples_format}",
-                "metadata": ui_extra_networks.ExtraNetworksPage.read_metadata_from_file(metadata_path),
+                "metadata": metadata,
             }
 
     def allowed_directories_for_previews(self):
         return [shared.cmd_opts.lora_dir]
-
